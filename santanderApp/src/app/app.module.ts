d52e868da522a00b79ba7c0624afac828126afd9
Allan Oliveira <allanolme@gmail.com>
2023-07-26 22:06:01 -0300

    create more components

--- santanderApp/src/app/app.module.ts
+++ santanderApp/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CardBoxComponent } from './components/card-box/card-box.component';
+import { AmountBoxComponent } from './components/amount-box/amount-box.component';
+import { CarouselItemsComponent } from './components/carousel-items/carousel-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    CardBoxComponent,
+    AmountBoxComponent,
+    CarouselItemsComponent
   ],
   imports: [
     BrowserModule,

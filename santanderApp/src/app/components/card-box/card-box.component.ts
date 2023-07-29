5033c04d9d04e235fbf23ecf677955066e95a469
Allan Oliveira <allanolme@gmail.com>
2023-07-29 20:59:50 -0300

    API working

--- santanderApp/src/app/components/card-box/card-box.component.ts
+++ santanderApp/src/app/components/card-box/card-box.component.ts
@@ -14,18 +14,28 @@ export class CardBoxComponent implements OnInit{
 	}
 
 	accountData: AccountDataModel = {
-	name: "Felipe",
-	account: {
-		agency: "0000",
-		number: "00.000000-0",
-	},
-	card: {
-		limit: 0,
-		number: "0000",
-	}
+		name: "Felipe",
+		account: {
+			agency: "0000",
+			number: "00.000000-0",
+		},
+		card: {
+			limit: 0,
+			number: "0000",
+		}
 	}
 	
 	ngOnInit(): void{
-		throw new Error('Method not implemented.')
+		this.getAccountData();
+	}
+	
+	getAccountData(){
+		this.service.getCard().subscribe(data => {
+			this.accountData.name = data.name;
+			this.accountData.account.agency = data.account.agency;
+			this.accountData.card.limit = data.card.limit;
+			this.accountData.account.number = data.account.number;
+			this.accountData.card.number = data.card.number.split(" ")[3];
+		})
 	}
 }

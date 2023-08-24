414b87458bc40276ebce241b923a7b08ec54b075
Allan Oliveira <allanolme@gmail.com>
2023-08-24 19:59:22 -0300

    Create types-preset.ts

---
+++ types-preset.ts
@@ -0,0 +1,17 @@
+type preset = boolean | string | number | any;
+
+let normal = "everything"
+
+let loading: preset;
+loading = false;
+
+let email: preset = "mail@email.com";
+
+let num: preset;
+num = 123
+
+let anyType: preset;
+anyType = "everything"
+
+//others types:
+//undefined, void, null, any[bigint, symbol, object]
